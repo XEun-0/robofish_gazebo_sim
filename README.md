b439037dd39d945aca2b5a8e9cd579b41830ae17
xeun0 <buttereflay@gmail.com>
2022-10-26 09:56:37 -0700

    u2

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Important Links to Consider
 http://masc.cs.gmu.edu/wiki/ROS
 
+CSULA Robotic Fish Research
--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/xeuno/Development/Robotfish/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/xeuno/Development/Robotfish/devel;/opt/ros/melodic'.split(';') if '/home/xeuno/Development/Robotfish/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
